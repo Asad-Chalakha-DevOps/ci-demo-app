d6f5f697f4fa66d07d84a0bc67fec109e38b9ae1
Asad-Chalakha-DevOps <asad.c@peerbits.com>
2025-06-09 15:20:24 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from GitHub Actions CI!");
+});
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
