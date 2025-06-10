3f073074053c8271554157517da76303b9dc1c04
Asad-Chalakha-DevOps <asad.c@peerbits.com>
2025-06-10 10:17:05 +0530

    testing

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from GitHub Actions CI/CD!");
+  res.end("Hello from GitHub Actions CI/cd!");
 });
 
 server.listen(port, () => {
